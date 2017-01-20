58295baf432d521d48a1f30f8b7de9a71509aec2
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-01-20 01:51:13 -0500

    Finished up logging

--- src/main/java/app/SimpleWebServer.java
+++ src/main/java/app/SimpleWebServer.java
@@ -1,9 +1,7 @@
 package app;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import server.Server;
+import utils.SwsLogger;
 
 /**
  * The entry point of the Simple Web Server (SWS).
@@ -11,8 +9,6 @@ import server.Server;
  * @author Chandan R. Rupakheti (rupakhet@rose-hulman.edu)
  */
 public class SimpleWebServer {
-	
-	final static Logger logger = LogManager.getLogger(SimpleWebServer.class);
 	
 	public static void main(String[] args) throws InterruptedException {
 		// TODO: Server configuration, ideally we want to read these from an application.properties file
@@ -27,7 +23,7 @@ public class SimpleWebServer {
 		
 		// DONE: Instead of just printing to the console, use proper logging mechanism.
 		// SL4J/Log4J are some popular logging framework
-		logger.debug(String.format("Simple Web Server started at port %d and serving the %s directory ...%n", port, rootDirectory));
+		SwsLogger.accessLogger.info(String.format("Simple Web Server started at port %d and serving the %s directory ...%n", port, rootDirectory));
 		
 		// Wait for the server thread to terminate
 		runner.join();
