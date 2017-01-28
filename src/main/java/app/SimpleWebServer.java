c1d4ee9755b144a959e7f6c3859f9b356169c4b8
Collin T Trowbridge <trowbrct@rose-hulman.edu>
2017-01-28 17:40:43 -0500

    add stupid error to see rollback work

--- src/main/java/app/SimpleWebServer.java
+++ src/main/java/app/SimpleWebServer.java
@@ -22,23 +22,24 @@ import java.util.Properties;
 public class SimpleWebServer {
 
 	public static void main(String[] args) throws Exception {
-		ServerProperties config = new ServerProperties();
-		Properties properties = config.getProperties("config.properties");
-
-		String rootDirectory = properties.getProperty("rootDirectory");
-		int port = Integer.parseInt(properties.getProperty("port"));
-
-		// Create a run the server
-		Server server = new Server(port, getPopulatedFactoryHash(rootDirectory));
-		Thread runner = new Thread(server);
-		runner.start();
-
-		// DONE: Instead of just printing to the console, use proper logging mechanism.
-		// SL4J/Log4J are some popular logging framework
-		SwsLogger.accessLogger.info(String.format("Simple Web Server started at port %d and serving the %s directory.", port, rootDirectory));
-		
-		// Wait for the unit.server thread to terminate
-		runner.join();
+		throw new Exception();
+//		ServerProperties config = new ServerProperties();
+//		Properties properties = config.getProperties("config.properties");
+//
+//		String rootDirectory = properties.getProperty("rootDirectory");
+//		int port = Integer.parseInt(properties.getProperty("port"));
+//
+//		// Create a run the server
+//		Server server = new Server(port, getPopulatedFactoryHash(rootDirectory));
+//		Thread runner = new Thread(server);
+//		runner.start();
+//
+//		// DONE: Instead of just printing to the console, use proper logging mechanism.
+//		// SL4J/Log4J are some popular logging framework
+//		SwsLogger.accessLogger.info(String.format("Simple Web Server started at port %d and serving the %s directory.", port, rootDirectory));
+//		
+//		// Wait for the unit.server thread to terminate
+//		runner.join();
 	}
 
 	public static HashMap<String, IRequestHandlerFactory> getPopulatedFactoryHash(String rootDirectory){
