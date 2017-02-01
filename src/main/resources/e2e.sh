bc2f0b805b36288c11fbdace2e0faf86234da032
Collin T Trowbridge <trowbrct@rose-hulman.edu>
2017-02-01 18:28:54 -0500

    bring e2e into resources directory. change config directories.

---
+++ src/main/resources/e2e.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+response=$(curl --write-out %{http_code} --silent localhost:8080)
+status_code=$(echo "$response" | sed -n '$p')
+case "$status_code" in
+	200) echo 'curl success!' \
+		 && rm -rf /home/csse/tmp
+		 ;;
+	  *) echo 'curl failed!' \
+	  	 && rm -rf /home/csse/YoloSwagSWS
+	  	 && mv /home/csse/tmp/YoloSwagSWS /home/csse \
+	  	 && rm -rf /home/csse/tmp \
+	  	 && service webserver stop \
+	  	 && service webserver start \
+	  	 && echo 'rolled back to previous version'
+	  	 exit 1
+	  	 ;;
+esac
