19d425ca9ec64fda30b49445d2e76ec6f5a9e9ef
Collin T Trowbridge <trowbrct@rose-hulman.edu>
2017-02-07 19:18:54 -0500

    update test to be async

--- src/test/java/benchmark/availability/ServerDowntimeTests.java
+++ src/test/java/benchmark/availability/ServerDowntimeTests.java
@@ -1,11 +1,16 @@
 package benchmark.availability;
 
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -22,16 +27,23 @@ public class ServerDowntimeTests {
 	public static void setUpBeforeClass() {
 		requestFactory = HTTP_TRANSPORT.createRequestFactory(request -> request.setParser(new JsonObjectParser(JSON_FACTORY)));
 	}
-	
+
 	@Test
 	public void testServerDowntimeToRestart() throws Exception {
 		GenericUrl url = new GenericUrl("http://477-19.csse.rose-hulman.edu:8080/serverexplosion.bat");
 		HttpRequest request = requestFactory.buildGetRequest(url);
+		Future<HttpResponse> f = request.executeAsync();
 		
 		try {
-			request.execute();
-		} catch (HttpResponseException e) {
-			System.err.println(e.getStatusCode());
+			long failedRequestSentTime = System.currentTimeMillis();
+			f.get(35, TimeUnit.MILLISECONDS);			
+		} catch (TimeoutException e) {
+			// swallow whole
+			// no spitting
 		}
+		
+//		while (true) {
+//			
+//		}
 	}
 }
